e6a38676b9d66a555615dd644350ce39148e4076
Vadim Khitrin <me@vkhitrin.com>
2025-10-01 21:52:45 +0300

    fix(routes): Stub `/containers/{id}/archive}`
    Signed-off-by: Vadim Khitrin <me@vkhitrin.com>

--- Sources/socktainer/Routes/ContainerArchiveRoute.swift
+++ Sources/socktainer/Routes/ContainerArchiveRoute.swift
@@ -2,7 +2,8 @@ import Vapor
 
 struct ContainerArchiveRoute: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        routes.get(":version", "containers", ":id", "archive", use: ContainerArchiveRoute.handler)
+        try routes.registerVersionedRoute(.GET, pattern: "/containers/{id:.*}/archive", use: ContainerArchiveRoute.handler)
+        try routes.registerVersionedRoute(.PUT, pattern: "/containers/{id:.*}/archive", use: ContainerArchiveRoute.handler)
     }
 
     static func handler(_ req: Request) async throws -> Response {
